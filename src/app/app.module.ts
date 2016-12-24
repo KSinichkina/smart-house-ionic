0e5fb8300e014e6e41ce2db55cabac9069d55c20
KSinichkina <sinichkinakate@gmail.com>
2016-12-24 18:57:50 +0200

    start ionic project

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AccountSettings } from '../pages/account-settings/account-settings';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AccountSettings,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AccountSettings,
+    ItemDetailsPage,
+    ListPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/account-settings/account-settings.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+import { Platform, ActionSheetController } from 'ionic-angular';
+
+@Component({
+  selector: 'account-settings',
+  templateUrl: 'account-settings.html'
+})
+export class AccountSettings {
+  constructor(
+      public platform: Platform,
+      public actionsheetCtrl: ActionSheetController
+  ) { }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Settings',
+      cssClass: 'account-settings',
+      buttons: [
+        {
+          text: 'Logout',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Logout clicked');
+          }
+        },
+        {
+          text: 'Delete Account',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+}
